5af1b19a52a5bd6b61cce2437ca4cabf3ce9a5ab
Slava Obrezkov <avito.golemtata@mail.ru>
2016-07-07 20:10:06 +0300

    Update DBOrganizationProject.cpp

--- DBOrganizationProject/DBOrganizationProject/DBOrganizationProject.cpp
+++ DBOrganizationProject/DBOrganizationProject/DBOrganizationProject.cpp
@@ -4,14 +4,14 @@ folder * root;
 
 int main()
 {
-  int a = 1;
-  root = (folder*)malloc(sizeof(folder));
-  root->UpFolder = NULL;
-  root->PreviousFolder = NULL;
-  root->NextFolder = (folder*)malloc(sizeof(folder));
-  root->File = (filet*)malloc(sizeof(filet));
-  root->File->Parametr = &a;
-  printf("lol");
-  printf("%d\n", *(int*)root->File->Parametr);
-  return 0;
+      int a = 1;
+      root = (folder*)malloc(sizeof(folder));
+      root->UpFolder = NULL;
+      root->PreviousFolder = NULL;
+      root->NextFolder = (folder*)malloc(sizeof(folder));
+      root->File = (filet*)malloc(sizeof(filet));
+      root->File->Parametr = &a;
+      printf("lol");
+      printf("%d\n", *(int*)root->File->Parametr);
+      return 0;
 }

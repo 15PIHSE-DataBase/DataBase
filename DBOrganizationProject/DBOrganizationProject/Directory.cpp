c08ee36b5c4f305f3affcc703fa6c951a36088b8
ascherdakova <ch.a.s.1997@mail.ru>
2016-07-07 19:44:44 +0300

    Некоторые комментарии

--- DBOrganizationProject/DBOrganizationProject/Directory.cpp
+++ DBOrganizationProject/DBOrganizationProject/Directory.cpp
@@ -1,12 +1,16 @@
+//Функция распечатывает содержимое текущей директории
+
 #include "stdafx.h"
 void Directory(FOLDER *CurrentF)
 {
 	FOLDER *temp;
+	//Проверка на наличие содержимого в данной директории
 	if (CurrentF->DownFolder!=NULL) temp = CurrentF->DownFolder;
 	else {
 		printf("\nDirectory is empty.\n");
 		return;
 	}
+	//Распечатка всех папок
 	printf("\nDirectory contains:\n");
 	while (1)
 	{

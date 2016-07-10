3179e0b665477e4955cd697b02af91ff8bcdb609
KirillDemochkin <dravkir@mail.ru>
2016-07-10 17:56:22 +0300

    Собрал прототипы для предрелиза

--- DBdemo/DBdemo/demoFunctions.h
+++ DBdemo/DBdemo/demoFunctions.h
@@ -1,1 +1,15 @@
 #pragma once
+
+void Delete(FOLDER*); //+ удаление
+
+FOLDER * DeleteFolders(FOLDER*);
+
+void Record(FOLDER*, FILE*); //+ сохранение
+
+void InputTree(FOLDER **); //+ создание
+
+void Instruction(); //?
+
+struct folder* scanfile(FILE*); //+ загрузка
+
+void way(folder*); //+ поиск

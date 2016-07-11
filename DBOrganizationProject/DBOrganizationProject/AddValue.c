b7d191e04558893ef2a63799bcd268f346d7d0c5
ascherdakova <ch.a.s.1997@mail.ru>
2016-07-11 22:57:43 +0300

    Модифицирование функции AddValue

--- DBOrganizationProject/DBOrganizationProject/AddValue.c
+++ DBOrganizationProject/DBOrganizationProject/AddValue.c
@@ -1,44 +1,44 @@
 #include"stdafx.h"
 //Добавление значения в узел.
-//Если добавление прошло удачно, функция вернёт значение исходного узла, иначе - NULL
-//NODE **CurrentPtr - адрес узла, в который помещается значение
-//int Type - тип помещаемого значения
+//Если добавление прошло удачно, функция вернёт указатель на исходный узел, иначе - NULL
+//NODE *CurrentPtr - Указатель на узел, в который помещается значение
+//TYPE Type - тип помещаемого значения (INT,FLOAT,DOUBLE,CHAR)
 //char * Info - помещаемое значение в виде строки
-NODE* AddValue(NODE **CurrentPtr, int Type, char * Info)
+NODE* AddValue(NODE *CurrentPtr, TYPE Type, char * Info)
 {
-	if ((Type < 0) || (Type>3))
+	if ((Type < 0) || (Type>3)) 
 		return NULL;
 	else
 	{
-		VALUE * Pointer = NULL;
+		VALUE * Pointer=NULL;
 		VALUE * NewValue = (VALUE*)malloc(sizeof(VALUE));
-		if (Pointer = (*CurrentPtr)->Values){
+		if (Pointer = CurrentPtr->Values){
 			while (Pointer->NextValue != NULL)
 				Pointer = Pointer->NextValue;
 			Pointer->NextValue = NewValue;
 		}
-		else (*CurrentPtr)->Values = NewValue; //Первое добавление данных
+		else CurrentPtr->Values = NewValue; //Первое добавление данных
 		NewValue->NextValue = NULL;
 		NewValue->Value = Info;
 		switch (Type){ //Определение типа
-		case 0:  NewValue->type = INT;  break;
-		case 1:  NewValue->type = FLOAT;  break;
-		case 2:  NewValue->type = DOUBLE;  break;
-		case 3:  NewValue->type = CHAR;  break;
+		case INT:  NewValue->type = INT;  break;
+		case FLOAT:  NewValue->type = FLOAT;  break;
+		case DOUBLE:  NewValue->type = DOUBLE;  break;
+		case CHAR:  NewValue->type = CHAR;  break;
 		}
-		//PrintKeys((*CurrentPtr)->Values); //Вывод всех данных узла с обновлениями
-		return *CurrentPtr;
+		//PrintValues(CurrentPtr->Values); //Вывод обновлённых значений узла
+		return CurrentPtr;
 	}
 }
-void PrintKeys(VALUE *head) //Вывод на экран всех значений узла и их типов
+void PrintValues(VALUE *head) //Вывод на экран всех значений узла и их типов
 {
 	while (head != NULL){
 		printf("%s ", head->Value);
 		switch (head->type){
-		case 0:printf("(int)  "); break;
-		case 1:printf("(float)  "); break;
-		case 2:printf("(double)  "); break;
-		case 3:printf("(char)  "); break;
+		case INT:printf("(int)  "); break;
+		case FLOAT:printf("(float)  "); break;
+		case DOUBLE:printf("(double)  "); break;
+		case CHAR:printf("(char)  "); break;
 		}
 		head = head->NextValue;
 	}

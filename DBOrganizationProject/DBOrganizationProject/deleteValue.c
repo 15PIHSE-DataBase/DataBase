efb8e910103156c854afbe3dcd48d7dcb090dbe8
TheMunchKingStudios <dravkir@mail.ru>
2016-07-12 18:51:47 -0400

    1

--- DBOrganizationProject/DBOrganizationProject/deleteValue.c
+++ DBOrganizationProject/DBOrganizationProject/deleteValue.c
@@ -6,10 +6,12 @@ int deleteVal(VALUE* toDelete) {
 		free(toDelete);
 		return 0;
 	}
-	VALUE *tmp = toDelete->NextValue;
-	toDelete->Value = tmp->Value;
-	toDelete->NextValue = tmp->NextValue;
-	toDelete->Qualifier = tmp->Qualifier;
-	free(tmp);
-	return 1;
+	else {
+		VALUE *tmp = toDelete->NextValue;
+		toDelete->Value = tmp->Value;
+		toDelete->NextValue = tmp->NextValue;
+		toDelete->Qualifier = tmp->Qualifier;
+		free(tmp);
+		return 1;
+	}
 }

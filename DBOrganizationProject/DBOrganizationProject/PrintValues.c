e3371f1e5affe8dc53b067a3c57fae0b5cc09d0b
ascherdakova <ch.a.s.1997@mail.ru>
2016-07-12 22:40:17 +0300

    Обновление PrintValues
    Изменение поступающего указателя

--- DBOrganizationProject/DBOrganizationProject/PrintValues.c
+++ DBOrganizationProject/DBOrganizationProject/PrintValues.c
@@ -1,18 +1,18 @@
 #include "stdafx.h"
 //Вывод на экран всех значений узла с их спецификаторами и типов этих значений.
 //Функция возвразает количество значений требуемого типа в указанном узле
-//VALUE *head - указатель на первое прикреплённое к узлу значение
+//VALUE * head - указатель на узел
 //TYPE Type - тип значений, которые нуобходимо вывести (ALL, INT, FLOAT, DOUBLE, CHAR)
-int PrintValues(VALUE *head, TYPE Type) 
+int PrintValues(NODE *head, TYPE Type) //Вывод на экран всех значений узла и их типов
 {
-	if (head == NULL) { //В узле нет значений
+	if (head->Values == NULL) {
 		printf("Empty\n");
 		return 0;
 	}
-	VALUE *temp = head;
+	VALUE *temp = head->Values;
 	int Count = 0;
-	while (temp != NULL){
-		if ((temp->type == Type) || (Type == ALL)){
+	while (temp != NULL){		
+		if ((temp->type == Type)||(Type==ALL)){
 			printf("%s: %s ", temp->Qualifier, temp->Value);
 			switch (temp->type){
 			case INT:printf("(int)  "); break;

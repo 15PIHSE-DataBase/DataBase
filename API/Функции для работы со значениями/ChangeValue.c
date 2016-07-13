fae4115e622b0387a369c68b4c5bb64408b26350
VasilyMatutin <vmatutin@gmail.com>
2016-07-13 17:58:32 +0300

    Повторный апдейт

--- API/Функции для работы со значениями/ChangeValue.c
+++ API/Функции для работы со значениями/ChangeValue.c
@@ -3,7 +3,7 @@
 void ChangeValue(VALUE* currPtr, TYPE Type, char* NewName, char* NewValue) //меняет значение, в т.ч. его тип: 1 указатель на значение, 2 тип, 3 новое имя, 4 новое значение
 {
 
-	if (Type != ALL)
+	if (Type != ALL) //если тип ALL, то не меняем тип
 	{
 		if (currPtr->type != Type)
 		{
@@ -16,8 +16,8 @@ void ChangeValue(VALUE* currPtr, TYPE Type, char* NewName, char* NewValue) //м�
 			}
 		}
 	}
-	if (NewName != NULL)
+	if (NewName != NULL)//если указатель NULL, то не меняем спецификатор
 		strcpy(currPtr->Qualifier, NewName);
-	if (NewValue != NULL)
+	if (NewValue != NULL)//если указатель NULL, то не меняем значение
 		strcpy(currPtr->Value, NewValue);
 }

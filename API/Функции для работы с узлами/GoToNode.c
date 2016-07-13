b1c66def1fed15b9c699309fee69abe3e7b6282f
Vasily Matutin <vmatutin@gmail.com>
2016-07-13 22:13:25 +0300

    Update GoToNode.c

--- API/Функции для работы с узлами/GoToNode.c
+++ API/Функции для работы с узлами/GoToNode.c
@@ -1,10 +1,8 @@
 NODE * goToNode(char* s1, NODE* node) {                 //выполняет переход в папку с данным именем из текущей директории
     if (!node) return NULL;
     NODE* tmp = node->DownNode;
-    char* s2;
     while (tmp != NULL) {               //ищем папку с именем, совпадающим с тем, которое подано в функцию
-        s2 = tmp->NodeName;
-        if (strcmp(s1, s2))
+        if (strcmp(s1, tmp->NodeName))
             tmp = tmp->NextNode;                    //если не нашли, то идем в следущего брата
         else
             return tmp;

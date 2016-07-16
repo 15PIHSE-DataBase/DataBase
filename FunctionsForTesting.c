6be8f3b12ba5cfb1038891a189d6a8abc2059307
ascherdakova <ch.a.s.1997@mail.ru>
2016-07-16 17:19:59 +0300

    Обновление FindError

--- FunctionsForTesting.c
+++ FunctionsForTesting.c
@@ -1,10 +1,21 @@
 void FindError(NODE * CurNode, char * ExprectedName)
 {
-	if (((!CurNode) && (!ExprectedName)) || (!strcmp(CurNode->NodeName, ExprectedName))) 
+	if ((!CurNode) || (!ExprectedName))
+		if ((!CurNode) && (!ExprectedName)) {
+			printf("It's OK\n"); return;
+		}
+		else {
+			printf("Error\n"); return;
+		}
+	else{
+		//if (((!CurNode) && (!ExprectedName)) || (!strcmp(CurNode->NodeName, ExprectedName)))  printf("It's OK\n");
+		if (!strcmp(CurNode->NodeName, ExprectedName))
 		{
 			printf("It's OK\n");
 			return;
 		}
 		else {
 			printf("Error\n"); return;
+		}
+	}
 }

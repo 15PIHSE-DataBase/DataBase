3c0db11abfc00681e68ac5ce0ccac8b3ebf7e45b
ascherdakova <ch.a.s.1997@mail.ru>
2016-07-16 20:56:33 +0300

    Обновление функций
    +добвлены функции: проверка работы ChangeNodeName и FullTree по
    распечатке всего дерева

--- FunctionsForTesting.c
+++ FunctionsForTesting.c
@@ -1,31 +1,24 @@
-﻿void FindError(NODE * CurNode, char * ExprectedName)
+﻿//Сравнивает имя указанного узла и ожидаемое имя
+void FindError(NODE * CurNode, char * ExprectedName) 
 {
 	if ((!CurNode) || (!ExprectedName))
-		if ((!CurNode) && (!ExprectedName)) {
-			printf("It's OK\n"); return;
-		}
-		else {
-			printf("Error\n"); return;
-		}
+		if ((!CurNode) && (!ExprectedName)) { printf("It's OK\n"); return;	}
+		else {	printf("Error\n"); return;	}
 	else{
 		if (!strcmp(CurNode->NodeName, ExprectedName))
-		{
-			printf("It's OK\n"); return;
-		}
-		else {
-			printf("Error\n"); return;
-		}
+		{ printf("It's OK\n"); return; }
+		else { printf("Error\n"); return; }
 	}
 }
-
-void BuildTree(NODE * root)
+//Построение дерева
+void BuildTree(NODE ** root)
 {
 	//Массивы имён для узлов
 	char name1[20] = "node1";
 	char name2[20] = "node2";//_2_3
 	char name3[20] = "node3";//_2_2
 	char name4[20] = "node4";//_2_1
-	char name5[20] = "node5";//_3_21_2
+	//char name5[20] = "node5";//_3_21_2
 	char name6[20] = "node6";//_3_21_1
 	char name7[20] = "node7";//_3_22_3
 	char name8[20] = "node8";//_3_22_2
@@ -33,37 +26,68 @@ void BuildTree(NODE * root)
 	(*root) = InputTree((*root), name1); 
 	NODE*pointer = NULL;
 	pointer = InputTree((*root), name2);
-	pointer = InputTree((*root), name3); 
-	pointer = InputTree((*root), name4);
+	pointer = InputTree((*root), name3);
+	pointer = InputTree((*root), name4); 
 	NODE *ptr = (*root)->DownNode;
-	pointer = InputTree(ptr, name5);
+	//pointer = InputTree(ptr, name5);
 	pointer = InputTree(ptr, name6);
 	ptr = ptr->NextNode;
-	pointer = InputTree(ptr, name7); CheckFunc(pointer);
-	pointer = InputTree(ptr, name8); CheckFunc(pointer);
-	pointer = InputTree(ptr, name9); CheckFunc(pointer);
+	pointer = InputTree(ptr, name7); //CheckFunc(pointer);
+	pointer = InputTree(ptr, name8); //CheckFunc(pointer);
+	pointer = InputTree(ptr, name9); //CheckFunc(pointer);
+	FullTree(*root); //Распечатка всех путей дерева
 }
+//Проверяет указатель на NULL (отладка)
 void CheckFunc(NODE * ptr)
 {
 	if (ptr)printf("%s\n", ptr->NodeName);
 	else printf("Error\n");
 }
-void ErrorSearch(char *NewName, NODE *CurPtr)
+//Проверка результатов PathList
+void FullTree(NODE * begin)
 {
-	//printf("%s\n", beginf->NodeName);
-	//printf("\n%d %s\n", count++, beginf->NodeName);
+	int print = PathList(begin);
+	if (print) printf("\n");
+	else printf("\nEmpty\n");
+}
+//Распечатка всех путей
+int PathList(NODE * CurrentNode)
+{
+	if (CurrentNode == NULL)
+		return NULL;//пустое дерево
+	PathList(CurrentNode->DownNode);
+	if (CurrentNode->DownNode == NULL)
+		way(CurrentNode);
+	PathList(CurrentNode->NextNode);
+	return 1;//1 если хоть что-то вывело
+}
+//Поиск ошибок в функции InsertTree, параметром funct необходимо указать именно её
+void ErrorSearch_InsertNode(char *NewName, NODE *CurPtr, NODE *funct(NODE*, char*))
+{
 	FindError(funct(CurPtr, NewName), NewName);
 	FindError(funct(CurPtr, NewName), NULL);
-
 	if (strcmp(CurPtr->NodeName, NewName) == 0)
 		return;
 	CurPtr = CurPtr->DownNode;
-	//printf("down: %s\n",  beginf->NodeName);
 	if (CurPtr != NULL)
 		while (CurPtr != NULL)
 		{
-			ErrorSearch(NewName, CurPtr);
+			ErrorSearch_InsertNode(NewName, CurPtr, funct);
 			CurPtr = CurPtr->NextNode;
 		}
 	return(NULL);
+}
+//Поиск ошибок в функции ChangeNodeName, параметром funct необходимо указать именно её
+void ErrorSearch_ChangeNodeName(char *NewName, NODE *CurPtr, NODE *funct(NODE*, char*))
+{
+	if (goToNode(NewName, CurPtr->UpNode)) //Такое имя уже есть в узле
+		FindError(funct(CurPtr, NewName), NULL);
+	else FindError(funct(CurPtr, NewName), NewName);
+	CurPtr = CurPtr->DownNode;
+	if (CurPtr != NULL)
+		while (CurPtr != NULL)
+		{
+			ErrorSearch_ChangeNodeName(NewName, CurPtr, funct);
+			CurPtr = CurPtr->NextNode;
+		}
 }

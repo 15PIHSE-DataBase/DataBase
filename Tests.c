c60f93c15ad92cc12c95b45d9968ac0ebb937fe0
ascherdakova <ch.a.s.1997@mail.ru>
2016-07-17 18:39:41 +0300

    проверка UpStep

--- Tests.c
+++ Tests.c
@@ -5,7 +5,20 @@ void main()
 {
 	NODE  *pointer = NULL;
 	NODE * MainRoot = NULL;
+
 	BuildTree(&MainRoot);
+	PrintLine();
+
+	printf("UpStep check...\n");			//NODE* UpStep(NODE*);
+	pointer = findnode("node8", MainRoot);
+	FindError(UpStep(NULL), NULL);
+	while (pointer->UpNode){
+		pointer = UpStep(pointer);
+		if (pointer)		
+			printf("%s ", pointer->NodeName);
+		else printf("\nThe end");
+	}
+	PrintLine();
 
 	printf("\nPress any key to exit\n");
 	getch();

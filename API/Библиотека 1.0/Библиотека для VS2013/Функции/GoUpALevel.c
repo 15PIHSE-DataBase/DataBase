caaf9557d8e2be9cc9db1eeb22479a44022ac394
VasilyMatutin <vmatutin@gmail.com>
2016-07-18 23:23:14 +0300

    Global clean

--- API/Библиотека 1.0/Библиотека для VS2013/Функции/GoUpALevel.c
+++
@@ -1,8 +0,0 @@
-﻿#include "DataBase15.h"
-
-NODE* up_step(NODE*Current)
-{
-	if (Current == NULL) return NULL;
-	if (Current->UpNode == NULL) return Current;
-	return Current->UpNode;
-}
--- API/Функции для работы с узлами/UpStep.c
+++
@@ -1,8 +0,0 @@
-//Функция осуществляет переход из текущей папки в папку на уровень выше - предок
-
-#include "stdafx.h"
-NODE* up_step(NODE*CurrentF) 
-{
-	if (CurrentF->UpNode == NULL) return CurrentF; //Корень 
-	return CurrentF->UpNode;
-}

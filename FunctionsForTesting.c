4e8fa42ec96f361b19d71bbcc7dcbf9fa892e1fd
ascherdakova <ch.a.s.1997@mail.ru>
2016-07-18 21:55:23 +0300

    Обновление функций тестирования
    Переименование функций под новый

--- FunctionsForTesting.c
+++ FunctionsForTesting.c
@@ -1,7 +1,7 @@
 ﻿#include"FunctionsForTesting.h"
 #include"DataBase13.h"
 //Сравнивает имя указанного узла и ожидаемое имя
-void FindError(NODE * CurNode, char * ExprectedName) 
+void find_error(NODE * CurNode, char * ExprectedName)
 {
 	if ((!CurNode) || (!ExprectedName))
 		if ((!CurNode) && (!ExprectedName)) { printf("It's OK\n"); return;	}
@@ -13,7 +13,7 @@ void FindError(NODE * CurNode, char * ExprectedName)
 	}
 }
 //Построение дерева
-void BuildTree(NODE ** root)
+void build_tree(NODE ** root)
 {
 	//Массивы имён для узлов
 	char name1[20] = "node1";
@@ -25,92 +25,86 @@ void BuildTree(NODE ** root)
 	char name7[20] = "node7";//_3_22_3
 	char name8[20] = "node8";//_3_22_2
 	char name9[20] = "node9";//_3_22_1
-	(*root) = InputTree((*root), name1); 
+	(*root) = input_tree((*root), name1); 
 	NODE*pointer = NULL;
-	pointer = InputTree((*root), name2);
-	pointer = InputTree((*root), name3);
-	pointer = InputTree((*root), name4); 
+	pointer = input_tree((*root), name2);
+	pointer = input_tree((*root), name3); 
+	pointer = input_tree((*root), name4); 
 	NODE *ptr = (*root)->DownNode;
 	//pointer = InputTree(ptr, name5);
-	pointer = InputTree(ptr, name6);
+	pointer = input_tree(ptr, name6);
 	ptr = ptr->NextNode;
-	pointer = InputTree(ptr, name7); //CheckFunc(pointer);
-	pointer = InputTree(ptr, name8); //CheckFunc(pointer);
-	pointer = InputTree(ptr, name9); //CheckFunc(pointer);
-	FullTree(*root); //Распечатка всех путей дерева
+	pointer = input_tree(ptr, name7);// CheckFunc(pointer);
+	pointer = input_tree(ptr, name8); //CheckFunc(pointer);
+	pointer = input_tree(ptr, name9); //CheckFunc(pointer);
+	full_tree(*root);
 }
 //Проверяет указатель на NULL (отладка)
-void CheckFunc(NODE * ptr)
+void check_func(NODE * ptr)
 {
 	if (ptr)printf("%s\n", ptr->NodeName);
 	else printf("Error\n");
 }
-//Проверка результатов PathList
-void FullTree(NODE * begin)
+//Проверка результатов path_list
+void full_tree(NODE * begin)
 {
-	if (!(PathList(begin))) printf("Empty\n");
+	if (!(path_list(begin))) printf("Empty\n");
 }
 //Линия разделения проверок
-void PrintLine(void)
+void print_line(void)
 {
 	for (int i = 0; i < 30; i++)
 		printf("=");
 	printf("\n");
 }
-//Распечатка всех путей
-int PathList(NODE * CurrentNode)
+//Поиск ошибок в функции insert_tree, параметром funct необходимо указать именно её
+void error_search_insert_node(char *NewName, NODE *CurPtr, NODE *funct(NODE*, char*))
 {
-	if (CurrentNode == NULL)
-		return NULL;//пустое дерево
-	PathList(CurrentNode->DownNode);
-	if (CurrentNode->DownNode == NULL)
-		way(CurrentNode);
-	PathList(CurrentNode->NextNode);
-	return 1;//1 если хоть что-то вывело
-}
-//Поиск ошибок в функции InsertTree, параметром funct необходимо указать именно её
-void ErrorSearch_InsertNode(char *NewName, NODE *CurPtr, NODE *funct(NODE*, char*))
-{
-	FindError(funct(CurPtr, NewName), NewName);
-	FindError(funct(CurPtr, NewName), NULL);
+	find_error(funct(CurPtr, NewName), NewName);
+	find_error(funct(CurPtr, NewName), NULL);
 	if (strcmp(CurPtr->NodeName, NewName) == 0)
 		return;
 	CurPtr = CurPtr->DownNode;
 	if (CurPtr != NULL)
 		while (CurPtr != NULL)
 		{
-			ErrorSearch_InsertNode(NewName, CurPtr, funct);
+			error_search_insert_node(NewName, CurPtr, funct);
 			CurPtr = CurPtr->NextNode;
 		}
 }
-//Поиск ошибок в функции ChangeNodeName, параметром funct необходимо указать именно её
-void ErrorSearch_ChangeNodeName(char *NewName, char*FindName, NODE *CurPtr, NODE *root)
+//Поиск ошибок в функции change_node_name, параметром funct необходимо указать именно её
+void error_search_change_node_name(char *NewName, char*FindName, NODE *CurPtr, NODE *root)
 {
-	NODE * temp = findnode(FindName, root);
+	NODE * temp = find_node(FindName, root);
 	if (!temp) return;
-	if (goToNode(NewName, UpStep(temp))) //Такое имя уже есть в узле
-		FindError(ChangeNodeName(temp, NewName), NULL);
-	else FindError(ChangeNodeName(temp, NewName), NewName);
+	if (go_to_node(NewName, up_step(temp))) //Такое имя уже есть в узле
+		find_error(change_node_name(temp, NewName), NULL);
+	else find_error(change_node_name(temp, NewName), NewName);
 	CurPtr = CurPtr->DownNode;
 	if (CurPtr != NULL)
 		while (CurPtr != NULL)
 		{
-			ErrorSearch_ChangeNodeName(NewName, FindName, CurPtr, root);
+			error_search_change_node_name(NewName, FindName, CurPtr, root);
 			CurPtr = CurPtr->NextNode;
 		}
 }
-//Поиск ошибок в функции Delete
-void ErrorSearch_DeleteNode(char *Name, NODE * MainRoot)
+//Поиск ошибок в функции delete_node
+void error_search_delete_node(char *Name, NODE ** MainRoot)
 {
-	NODE * temp;
-	while (temp = findnode(Name, MainRoot)){
-		if (!Delete(&temp, &MainRoot))
-			printf("Nothing to delete\n");
-		if (!MainRoot) break;
+	if (!(*MainRoot)){
+		printf("Wrong pointer\n");
+		return;
 	}
+	NODE * temp;
+	while (temp = find_node(Name, *MainRoot)){
+		if (!delete_node(&temp, MainRoot))
+			printf("Nothing to delete\n");
+		if (!(*MainRoot)) break;
+	}
+
 }
 //Сравнивает информацию указанного значения с ожидаемыми
-void FindErrorInValues(VALUE * Current, char * ExpectedValue, char*ExpectedQualifier, TYPE ExpectedType)
+void find_error_in_values(VALUE * Current, char * ExpectedValue, char*ExpectedQualifier, TYPE ExpectedType)
 {
 	if ((!Current) || (!ExpectedValue) || (!ExpectedQualifier))
 		if ((!Current) && (!ExpectedValue) && (!ExpectedQualifier)) { printf("It's OK\n"); return; }
@@ -123,45 +117,46 @@ void FindErrorInValues(VALUE * Current, char * ExpectedValue, char*ExpectedQuali
 		else { printf("Error\n"); return; }
 	}
 }
-//Поиск ошибок в функции AddValue
-void ErrorSearch_AddValue(NODE * node)
+//Поиск ошибок в функции add_value
+void error_search_add_value(NODE * node)
 {
-	char Value[3][10] = { "100y", "red", "forest" }, Qualif[3][10] = { "Age", "Color", "Place" };
-	for (int i = 0; i < 3; i++){
-		FindErrorInValues(AddValue(node, Qualif[i], INT, Value[i]), Value[i], Qualif[i], INT);
-		FindErrorInValues(AddValue(node, Qualif[i], INT, Value[i]), NULL, NULL, INT);
+	char Value[4][10] = { "100y", "red", "forest", "1m" }, Qualif[4][10] = { "Age", "Color", "Place", "Size" };
+	for (int i = 0; i < 4; i++){
+		find_error_in_values(add_value(node, Qualif[i], INT, Value[i]), Value[i], Qualif[i], INT);
+		find_error_in_values(add_value(node, Qualif[i], INT, Value[i]), NULL, NULL, INT);
 	}
 }
 //Поиск ошибок в функциях поиска и удаления значения узла
-void ErrorSearch_FindDeleteValue(NODE * node)
+void error_search_find_delete_value(NODE * node)
 {
 	char Qualif[4][10] = { "Age", "Color", "Place", "Size" }, Value[4][10] = { "100y", "red", "forest", "1m" };
 	VALUE * temp; int result;
-	temp = findValueInNode(node, Qualif[0]);
-	FindErrorInValues(temp, Value[0], Qualif[0], INT);
-	result = deleteVal(temp, node);
-	PrintValues(node, ALL);
+	temp = find_value_in_node(node, Qualif[0]);
+	find_error_in_values(temp, Value[0], Qualif[0], INT);
+	result = delete_val(temp, node);
+	print_values(node, ALL);
 
-	temp = findValueInNode(node, Qualif[2]);
-	FindErrorInValues(temp, Value[2], Qualif[2], INT);
-	result = deleteVal(temp, node);
-	PrintValues(node, ALL);
+	temp = find_value_in_node(node, Qualif[2]);
+	find_error_in_values(temp, Value[2], Qualif[2], INT);
+	result = delete_val(temp, node);
+	print_values(node, ALL);
 
-	temp = findValueInNode(node, Qualif[3]);
-	FindErrorInValues(temp, Value[3], Qualif[3], INT);
-	result = deleteVal(temp, node);
-	PrintValues(node, ALL);
+	temp = find_value_in_node(node, Qualif[3]);
+	find_error_in_values(temp, Value[3], Qualif[3], INT);
+	result = delete_val(temp, node);
+	print_values(node, ALL);
 
-	FindErrorInValues(findValueInNode(node, Qualif[3]), NULL, NULL, INT); //провека на поиск несуществующего спецификатора
+	find_error_in_values(find_value_in_node(node, Qualif[3]), NULL, NULL, INT); //провека на поиск несуществующего спецификатора
 	/*
-	temp = findValueInNode(node, Qualif[1]);
-	FindErrorInValues(temp, Value[1], Qualif[1], INT);
-	result = deleteVal(temp, node);
-	PrintValues(node, ALL);
+	temp = find_value_in_node(node, Qualif[1]);
+	find_error_in_values(temp, Value[1], Qualif[1], INT);
+	result = delete_val(temp, node);
+	print_values(node, ALL);
 	*/
 }
-void ErrorSearch_DeleteAllValues(NODE * node)
+//Поиск ошибок в функциях поиска и удаления значения узла
+void error_search_delete_all_values(NODE * node)
 {
-	DeleteAllValues(node, INT);
-	PrintValues(node, INT);
+	delete_all_value(node, INT);
+	print_values(node, INT);
 }
--- Tests.c
+++ Tests.c
@@ -10,78 +10,81 @@ void main()
 		UpExpect[2][10] = { "node3", "node1" },
 		path[5][30] = { "node1", "node1.node3", "node1.node4.node6.NewNode1", "node1.node8", "12.34" },
 		PathExpect[3][10] = { "node1", "node3", "NewNode1" };
-	BuildTree(&MainRoot);
-	//Directory(NULL);
-	PrintLine();
-	printf("UpStep check...\n");		//NODE* UpStep(NODE*);
-	pointer = findnode("node8", MainRoot);
-	FindError(UpStep(NULL), NULL);
+	build_tree(&MainRoot);
+	//directory(NULL);
+	print_line();
+	printf("up_step check...\n");			//NODE* up_step(NODE*);
+	pointer = find_node("node8", MainRoot);
+	find_error(up_step(NULL), NULL);
 	int i = 0;
 	while (pointer->UpNode){
-		FindError(pointer = UpStep(pointer), UpExpect[i]);
+		find_error(pointer = up_step(pointer), UpExpect[i]);
 		i++;
 	}
-	PrintLine();
+	print_line();
 
-	printf("InputTree check...\n");		// NODE* InputTree(NODE *, char *);
-	ErrorSearch_InsertNode(NewName1, MainRoot, InputTree);
-	FullTree(MainRoot);
-	PrintLine();
+	printf("input_tree check...\n");			// NODE* input_tree(NODE *, char *);
+	error_search_insert_node(NewName1, MainRoot, input_tree);
+	full_tree(MainRoot);
+	print_line();
 
-	printf("goToPath check...");		//NODE* goToPath(NODE *, char*);
+	printf("go_to_path check...");			//NODE* go_to_path(NODE *, char*);
 	for (i = 0; i < 3; i++)
-		FindError(goToPath(MainRoot, path[i]), PathExpect[i]);
+		find_error(go_to_path(MainRoot, path[i]), PathExpect[i]);
 	for (i = 3; i < 5; i++)
-		FindError(goToPath(MainRoot, path[i]), NULL);
-	PrintLine();
+		find_error(go_to_path(MainRoot, path[i]), NULL);
+	print_line();
 
-	printf("ChangeNodeName check...\n");	//NODE* ChangeNodeName(NODE * CurrentNode, char *NewName);
-	ErrorSearch_ChangeNodeName(NewName2, NewName1, MainRoot, MainRoot);
-	FullTree(MainRoot);
-	PrintLine();
+	printf("change_node_name check...\n");	//NODE* change_node_name(NODE * CurrentNode, char *NewName);
+	error_search_change_node_name(NewName2, NewName1, MainRoot, MainRoot);
+	full_tree(MainRoot);
+	print_line();
 
-	printf("findnode check...\n");			//NODE* findnode(char*, NODE*);
-	FindError(findnode(NewName1, MainRoot), NULL);
-	FindError(findnode(NewName2, MainRoot), NewName2);
-	FindError(findnode("node4", MainRoot->DownNode), NULL);
-	FindError(findnode("node4", MainRoot), "node4");
-	PrintLine();
+	printf("find_node check...\n");			//NODE* find_node(char*, NODE*);
+	find_error(find_node(NewName1, MainRoot), NULL);
+	find_error(find_node(NewName2, MainRoot), NewName2);
+	find_error(find_node("node4", MainRoot->DownNode), NULL);
+	find_error(find_node("node4", MainRoot), "node4");
+	print_line();
 
-	printf("Delete check...\n");			//int Delete(NODE**, NODE**);
+	printf("delete_node check...\n");			//int delete_node(NODE**, NODE**);
 	i = 1; printf("\n>>Check %d<<\n", i++);
-	ErrorSearch_DeleteNode(NewName2, &MainRoot);
-	FullTree(MainRoot);
+	error_search_delete_node(NewName2, &MainRoot);
+	full_tree(MainRoot);
 	printf("\n>>Check %d<<\n", i++);
-	ErrorSearch_DeleteNode("node3", &MainRoot);
-	FullTree(MainRoot);
+	error_search_delete_node("node3", &MainRoot);
+	full_tree(MainRoot);
 	printf("\n>>Check %d<<\n", i++);
-	ErrorSearch_DeleteNode("node1", &MainRoot);
-	FullTree(MainRoot);
+	error_search_delete_node("node1", &MainRoot);
+	full_tree(MainRoot);
 	printf("\n>>Check %d<<\n", i++);
-	ErrorSearch_DeleteNode("node1", &MainRoot);
-	FullTree(MainRoot);
-	PrintLine();
+	error_search_delete_node("node1", &MainRoot);
+	full_tree(MainRoot);
+	print_line();
 	//============================== V A L U E S ==============================
-	BuildTree(&MainRoot);
-	PrintLine();
+	build_tree(&MainRoot);
+	print_line();
 	char Value[3][10] = { "100y", "red", "forest" }, Qualif[3][10] = { "Age", "Color", "Place" };
 	pointer = MainRoot;
 
-	printf("AddValue check...\n");						// NODE* InputTree(NODE *, char *);
-	ErrorSearch_AddValue(pointer);
-	PrintValues(pointer, ALL);
-	PrintLine();
+	printf("add_value check...\n");							// VALUE * add_value(NODE *CurrentPtr, char *Qualf, TYPE Type, char * Info)
+	error_search_add_value(pointer);
+	error_search_add_value(pointer->DownNode);
+	print_values(pointer, ALL);
+	print_line();
 
-	printf("PrintValues check...\n");					//void PrintValues(NODE*, TYPE);
-	PrintValues(pointer, ALL);
-	PrintValues(pointer->DownNode, ALL);
-	PrintLine();
+	printf("print_values check...\n");						//void print_values(NODE*, TYPE);
+	print_values(pointer, ALL);
+	print_values(pointer->DownNode, ALL);
+	print_line();
 
-	printf("findValueInNode & deleteValues check...\n"); //VALUE* findValueInNode(NODE*, char*); int deleteVal(VALUE*, NODE*);
-	ErrorSearch_FindDeleteValue(pointer);
+	printf("find_value_in_node & delete_val check...\n");	//VALUE* find_value_in_node(NODE*, char*); int delete_val(VALUE*, NODE*);
+	error_search_find_delete_value(pointer);
+	print_line();
 
-	printf("DeleteAllValue check...\n");					//void DeleteAllValue(NODE *, TYPE);
-	ErrorSearch_DeleteAllValues(pointer);
+	printf("delete_all_value check...\n");					//void delete_all_value(NODE *, TYPE);
+	error_search_delete_all_values(pointer);
+	print_line();
 
 	printf("\nPress any key to exit\n");
 	getch();

ab02ec72d7c56d1713dbebb4db4dc772ddb6d22d
TheMunchKingStudios <dravkir@mail.ru>
2016-07-17 21:26:06 -0400

    Вынес библиотеку для VS15 в отдельную папку

---
+++ API/Библиотека 1.0/Библиотека для VS2015/Функции/CopyValue.c
@@ -0,0 +1,37 @@
+﻿
+#include "DataBase15.h"
+
+VALUE* CopyPasteValue(NODE *PtrFrom, VALUE *temp, NODE *PtrTo, int* i) //функция cutpuste, copypaste. функция принимает указатель на узел из которого скопировать,
+																	   //адрес копируемого значения и указатель на узел куда нужно вставить значение, а также режим 0-копия 1-вырезать-вставить
+																	   //вернёт адрес вставленного элемента в случае успеха или NULL в случае ошибки. В i код ошибки: 2 в случае попытки вставить в узел где уже есть значение с таким именем и 3 при выборе неправильного режима
+{
+	VALUE* temp2 = temp;
+	VALUE *p = NULL;
+	switch (*i)
+	{
+	case 0:
+		p = AddValue(PtrTo, temp->Qualifier, temp->type, temp->Value);
+		if (p != NULL)
+		{
+			return (p);
+		}
+		else
+			return(NULL);
+		i = 2;
+		break;
+	case 1:
+		deleteVal(temp, PtrFrom);
+		p = AddValue(PtrTo, temp->Qualifier, temp->type, temp->Value);
+		if (p != NULL)
+		{
+			return (p);
+		}
+		else
+			return(NULL);
+		i = 2;
+		break;
+	default:
+		return(NULL);
+		i = 3;
+	}
+}
---
+++ API/Библиотека 1.0/Библиотека для VS2015/Функции/GetQualifier.c
@@ -0,0 +1,13 @@
+﻿
+#include "DataBase15.h"
+
+char* GetQualifier(VALUE * pointer)
+{
+	if (pointer)
+	{
+		char *TempString = (char*)malloc(strlen(pointer->Qualifier) + 1);
+		strcpy(TempString, pointer->Qualifier);
+		return TempString;
+	}
+	else return NULL; //указатель пуст
+}
